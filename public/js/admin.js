0fd6b29f3d40f698d6f5ec6daad4a5aecf3c10ea
Adriano Miranda <adrjmiranda@gmail.com>
2024-06-20 11:47:22 -0300

    chore: created admin login page

---
+++ public/js/admin.js
@@ -0,0 +1,43 @@
+// Pass Visibility
+
+const passInput = document.querySelector('#password');
+const passConfirmationInput = document.querySelector('#password-confirmation');
+
+const toggleVisibilityPassButton = document.querySelectorAll(
+	'.toggle-visibility-pass'
+);
+
+function toggleHide(element) {
+	element.classList.toggle('hide');
+}
+
+function toogglePassVisibility(parent, element) {
+	let inputType = element.getAttribute('type');
+	let newType = inputType === 'text' ? 'password' : 'text';
+
+	element.setAttribute('type', newType);
+
+	let visibleButton = parent.querySelector('.visible');
+	let hiddenButton = parent.querySelector('.hidden');
+
+	toggleHide(visibleButton);
+	toggleHide(hiddenButton);
+}
+
+toggleVisibilityPassButton &&
+	toggleVisibilityPassButton.forEach((btn) => {
+		btn.addEventListener('click', () => {
+			const dataArea = btn.dataset.area;
+
+			switch (dataArea) {
+				case 'pass':
+					passInput && toogglePassVisibility(btn, passInput);
+					break;
+
+				case 'passconfirmation':
+					passConfirmationInput &&
+						toogglePassVisibility(btn, passConfirmationInput);
+					break;
+			}
+		});
+	});
